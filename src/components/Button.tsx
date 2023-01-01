6d8a94b53073ca88412ccba31b4a5184db9581cb
Silas Martins <silas.martins2041@gmail.com>
2023-01-01 14:53:48 -0300

    Adicionando DarkMode

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+interface ButtonProps {
+  children: any;
+  className: string;
+  onClick: any;
+}
+
+export default function Button({children, className, onClick}:ButtonProps) {
+  return (
+    <>
+      <button className={`p-2 rounded-md hover:ring-2 mr-16 ${className}`} onClick={onClick}>
+        {children}     
+      </button>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/components/**/*.tsx',
     './src/pages/**/*.tsx'
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
