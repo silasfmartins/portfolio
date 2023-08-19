597007f4235a08543f6574544f82b05ae76ebd7c
Silas Martins <silas.martins2041@gmail.com>
2023-08-18 21:28:50 -0300

    Update HomeHero

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -40,7 +40,7 @@ export function HomeHero({ homeInfo }: HeroHomeProps) {
           <span className="font-sans text-lg text-gray-700 dark:text-gray-300">
             <Typewriter
               options={{
-                strings: ['Analista de Sistemas na Vivo'],
+                strings: ['Estagiário de Análise de Sistemas na Vivo'],
                 autoStart: true,
                 loop: true,
               }}
